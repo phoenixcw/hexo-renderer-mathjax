eb1e745fdfaac428719e28cfcecfec6a02122dc7
AdrieanKhisbe <adriean.khisbe@live.fr>
2015-12-21 09:04:05 +0100

    Remove callback

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ var layout = 'layout.ejs';
 var bodyTag = '</body>';
 var mathjaxScript = fs.readFileSync(path.join(__dirname, 'mathjax.html'));
 
-hexo.extend.renderer.register('ejs', 'html', function(data, options, callback) {
+hexo.extend.renderer.register('ejs', 'html', function(data, options) {
     var path = options.filename = data.path;
     var content = data.text;
     if (layout === path.substring(path.length - layout.length))
         content = content.replace(bodyTag, mathjaxScript + '\n' + bodyTag);
-    callback(null, ejs.render(content, options));
+    ejs.render(content, options);
 });

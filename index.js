c58de5f444a4759c72a3aa75523f467ff16e70ae
Rapiz <contact@rapiz.me>
2020-08-23 12:50:53 +0800

    Add missing quotes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ hexo.extend.renderer.register('ejs', 'html', function(data, options) {
     var path = options.filename = data.path;
     var content = data.text;
     if (!isEmpty(hexo.config.mathjax) && !isEmpty(hexo.config.mathjax.cdn)) {
-        content = content.replace(bodyTag, mathjaxScript +'\n' +'<script src=' + hexo.config.mathjax.cdn + '?config=TeX-AMS-MML_HTMLorMML></script>' +'\n' + bodyTag);
+        content = content.replace(bodyTag, mathjaxScript +'\n' +'<script src="' + hexo.config.mathjax.cdn + '?config=TeX-AMS-MML_HTMLorMML"></script>' +'\n' + bodyTag);
     }else{
         content = content.replace(bodyTag, mathjaxScript
         +'<script src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>' +'\n' + bodyTag);
